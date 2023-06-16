e211d97a6e82765337fa27c28b3ffe953658090b
rathodrutu548 <98386300+rathodrutu548@users.noreply.github.com>
2023-06-16 12:35:57 +0530

    Add files via upload
    This file not containing any code . There are 2 ways to convert python file to exe file both way are descripted here you have to write command on your commmand prompt to see the proper output

---
+++ py to exe_convert.py
@@ -0,0 +1,8 @@
+# way 1
+# step1 open cmd in python file folder(the file we want to conert)
+# step2 write pyinstaller --onefile (file name)
+
+# way 2
+# step1 step one is same
+# step 2 write auro-py-to-exe
+# in this icon should have .ioc format
