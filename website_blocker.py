5284c9fdcd16e230d60edd6ea6b58d4a53b62a27
rathodrutu548 <98386300+rathodrutu548@users.noreply.github.com>
2023-06-16 13:32:32 +0530

    Update website_blocker.py

--- website_blocker.py
+++ website_blocker.py
@@ -4,8 +4,8 @@ root.geometry('500x300')
 root.resizable(0,0)
 root.title("Website Blocker")
 Label(root, text ='Website Blocker' , font ='arial 20 bold').pack()
-host_path ='C:\Windows\System32\drivers\etc\hosts'
-ip_address = '127.0.0.1'
+host_path ='Your host file path'
+ip_address = 'your ip addres'
 Label(root, text ='Enter Website :' , font ='arial 13 bold').place(x=5 ,y=60)
 Websites = Text(root,font = 'arial 10',height='2', width = '40')
 Websites.place(x= 140,y = 60)
